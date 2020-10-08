ae29713cec6b14e9618525130271e239f81916e2
ALBERTO VISA FLORES <albertovisaf@gmail.com>
2020-10-07 22:28:50 -0500

    avance de ptears

---
+++ lab2/parteI/compilar.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+gcc -g -Wall -o pth_pi_busy2 pth_pi_busy2.c -lm -lpthread
+./pth_pi_busy2 2 100000000
+./pth_pi_busy2 4 100000000 
+./pth_pi_busy2 8 100000000
+./pth_pi_busy2 16 100000000
+./pth_pi_busy2 32 100000000
+./pth_pi_busy2 64 100000000
+
