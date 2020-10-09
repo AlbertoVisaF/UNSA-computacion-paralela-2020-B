b0b6210cd5b83ba1550f202d0ca0bb4af730545d
ALBERTO VISA FLORES <albertovisaf@gmail.com>
2020-10-09 03:40:23 -0500

    script ordenado

--- prac2/compilar.sh
+++ prac2/compilar.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 gcc -g -Wall -o mat_vec pth_mat_vec.c -lpthread
-   ./mat_vec 1  8000000 8
-   ./mat_vec 1  800 800
-   ./mat_vec 1 8 8000000
-  ./mat_vec 2  8000000 8
- ./mat_vec 2  800 800
-  ./mat_vec 2 8 8000000
- ./mat_vec 4  8000000 8
-  ./mat_vec 4  800 800
-   ./mat_vec 4 8 8000000
+echo "1 800000*8"
+./mat_vec 1  8000000 8
+echo "2 800000*8"
+./mat_vec 2  8000000 8
+echo "4 800000*8"
+./mat_vec 4  8000000 8
+echo "1 800*800"
+./mat_vec 1  800 800
+echo "2 800*800"
+./mat_vec 2  800 800
+echo "4 8 8000000"
+./mat_vec 4  800 800
+echo "1 8*800000"
+./mat_vec 1 8 8000000
+echo "2 8*800000"
+./mat_vec 2 8 8000000
+echo "4 8*800000"
+./mat_vec 4 8 8000000
  
 
